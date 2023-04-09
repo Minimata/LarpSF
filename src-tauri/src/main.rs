334bad3f365d8184b9873f0b49622b48af132554
Minimata <minimata94@gmail.com>
2023-04-09 15:18:26 +0200

    Computing on rust backend, forwaring to UI and firestore

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,61 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::Window;
+use tokio::time;
+use std::collections::HashMap;
+
+
+#[derive(Clone, serde::Serialize)]
+struct Ship {
+  energy: f64,
+  energy_growth: f64,
+  food: f64,
+  food_growth: f64,
+  oxygen: f64,
+  oxygen_growth: f64,
+}
+
+
+#[tauri::command]
+async fn game_loop(window: Window, ship_values: HashMap<String, f64>){
+  let mut ship = Ship {
+    energy: ship_values["energy"],
+    energy_growth: ship_values["energy_growth"],
+    food: ship_values["food"],
+    food_growth: ship_values["food_growth"],
+    oxygen: ship_values["oxygen"],
+    oxygen_growth: ship_values["oxygen_growth"],
+  };
+
+  let mut interval = time::interval(time::Duration::from_secs(1));
+  loop {
+    interval.tick().await;
+    
+    let energy_growth_ppm = ship.energy_growth / 1000000.0;
+    let food_growth_ppm = ship.food_growth / 1000000.0;
+    let oxygen_growth_ppm = ship.oxygen_growth / 1000000.0;
+
+    ship.energy += energy_growth_ppm;
+    ship.food += food_growth_ppm;
+    ship.oxygen += oxygen_growth_ppm;
+
+    window.emit("ShipProgress", Ship {
+      energy: ship.energy,
+      energy_growth: ship.energy_growth,
+      food: ship.food,
+      food_growth: ship.food_growth,
+      oxygen: ship.oxygen,
+      oxygen_growth: ship.oxygen_growth,
+    }).unwrap();
+  } 
+}
+
+
 fn main() {
   tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+  .invoke_handler(tauri::generate_handler![game_loop])
+  .run(tauri::generate_context!())
+  .expect("error while running tauri application");
 }
+
--- src/components/ShipValuesPreview.vue
+++ src/components/ShipValuesPreview.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, doc } from "firebase/firestore";
+import { getFirestore, collection, doc, setDoc } from "firebase/firestore";
 import { useCollection, useDocument } from "vuefire";
+
+import { invoke } from "@tauri-apps/api/tauri";
+import { emit, listen } from "@tauri-apps/api/event";
+import { appWindow } from "@tauri-apps/api/window";
 
 const firebaseApp = initializeApp({
   apiKey: import.meta.env.FIREBASE_API_KEY,
@@ -14,17 +17,43 @@ const firebaseApp = initializeApp({
   measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID,
 });
 
+interface Ship {
+  energy: number;
+  energy_growth: number;
+  food: number;
+  food_growth: number;
+  oxygen: number;
+  oxygen_growth: number;
+}
+
 // used for the firestore refs
 const db = getFirestore(firebaseApp);
-
 // here we can export reusable database references
-const shipDoc = useDocument(doc(collection(db, "ship"), "ship"));
+const shipFireDoc = doc(collection(db, "ship"), "ship");
+const shipDoc = useDocument(shipFireDoc);
 
-const value = ref(0);
+const buttonClicked = async () => {
+  console.log(shipDoc);
 
-const buttonClicked = () => {
-  value.value++;
+  const shipValues = {
+    energy: shipDoc?.value?.energy,
+    energy_growth: shipDoc?.value?.energy_growth,
+    food: shipDoc?.value?.food,
+    food_growth: shipDoc?.value?.food_growth,
+    oxygen: shipDoc?.value?.oxygen,
+    oxygen_growth: shipDoc?.value?.oxygen_growth,
+  };
+  const result = await invoke("game_loop", {
+    window: appWindow,
+    shipValues: shipValues,
+  });
 };
+
+const unlistenProgress = appWindow.listen("ShipProgress", ({ payload }) => {
+  const shipValues = payload as Ship;
+  setDoc(shipFireDoc, shipValues);
+  shipDoc!.value! = shipValues;
+});
 </script>
 
 <template>
@@ -36,7 +65,7 @@ const buttonClicked = () => {
       class="btn btn-outline btn-secondary row-start-1 col-start-2 justify-self-start self-center"
       @click="buttonClicked"
     >
-      Add to values
+      Start simulation
     </button>
 
     <div
@@ -44,17 +73,17 @@ const buttonClicked = () => {
     >
       <progress
         class="progress progress-accent w-full"
-        :value="shipDoc!.energy * 100"
+        :value="shipDoc?.energy * 100"
         max="100"
       ></progress>
       <progress
         class="progress progress-success w-full"
-        :value="shipDoc!.food * 100"
+        :value="shipDoc?.food * 100"
         max="100"
       ></progress>
       <progress
         class="progress progress-primary w-full"
-        :value="shipDoc!.oxygen * 100"
+        :value="shipDoc?.oxygen * 100"
         max="100"
       ></progress>
     </div>
